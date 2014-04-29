5b70608381b3c2f278ff98bcebf5374b49d3cfb4
Hassan Hayat <hassan.hayat7@gmail.com>
2014-04-29 01:37:28 +0100

    Made example work
    Example is functional and more composable

--- main.elm
+++ main.elm
@@ -1,6 +1,27 @@
 import Physics.Math.Point (..)
 import Physics.Body (..)
+import Keyboard
 
-hero = dynamicBody 40 origin origin
+hero = dynamicBody 50 origin origin
 
-main = asText (applyForce earthGravity hero)
+
+drawBody : Body a b -> Element
+drawBody body = collage 400 400 
+                [move (body.position.x , body.position.y) (filled red (circle 50))]  
+
+render : Signal (Body a b) -> Signal Element
+render body = lift drawBody body
+
+
+getInput : Signal (Point {})
+getInput = lift (scaleBy 100) (lift toFloatPoint Keyboard.arrows)
+
+
+updatePhysics : Body a b -> Signal (Point c) -> Signal (Body a b)
+updatePhysics = foldp applyForceNow 
+
+update : Body a b -> Signal Element
+update body = getInput |> updatePhysics body |> render
+
+main : Signal Element
+main = update hero
